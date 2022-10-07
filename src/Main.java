03cdddaf88434dba8c08ad02cb42fd8e710fc097
kingben001 <kingben001@yahoo.com>
2022-10-07 19:57:10 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue myQueue = new Queue(2);
+
+        myQueue.enqueue(1);
+
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue());
+
+
+       myQueue.printQueue();
+    }
+}
